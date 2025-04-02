e9a438f9dd22e758bb4fd74c17b5905a00008224
Bayez77 <mrbayez88@gmail.com>
2025-04-02 11:23:43 -0500

    Add project files.

---
+++ Models/Authors.cs
@@ -0,0 +1,14 @@
+﻿namespace simplyBooksBE.Models
+{
+    public class Authors
+    {
+        public int ID { get; set; }
+        public string? Uid { get; set; }
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public string? Email { get; set; }
+        public bool Favorite { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,14 @@
+﻿namespace simplyBooksBE.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public int Author_Id { get; set; }
+        public string? Uid { get; set; }
+        public string? Title { get; set; }
+        public int Price { get; set; }
+        public bool Sale { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Description { get; set; }
+    }
+}
